4ca2bb1d4ada570aec23b24ec0c6275b33df05b9
AndPdb <and.pdb92@gmail.com>
2019-05-10 15:34:52 +0200

    Description of the script

--- vina.sh
+++ vina.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+### Script for virtual screening. It shuld be run in the same folder of the receptor and conf.txt. ligands shuld be in a subfolder named ligands, the config file shuld be named conf.txt, and the output folder named output.
+
 VINAPATH=/home/andreap/Programs/AutodockVina/vina
 LIG=./ligands/*.pdbqt
 OUT=./output
