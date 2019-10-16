d1801a90c60b53189697e8982305ae81ec85e9bf
AndPdb <and.pdb92@gmail.com>
2019-10-16 15:18:54 +0200

    Fix path.join and ligand merge

--- Vina_merge.py
+++ Vina_merge.py
@@ -11,6 +11,7 @@ from io import StringIO
 import subprocess
 import argparse
 import os
+from os import path
 
 
 ###SOME PATHS###
@@ -19,31 +20,31 @@ MGLTOOLSDIR = "/home/andrea/Programs/mgltools_x86_64Linux2_1.5.7rc1/"
 
 #path for utilities and pythonsh of mgltools. In linux OS these should be the same
 utilities24 = "{0}MGLToolsPckgs/AutoDockTools/Utilities24/".format(MGLTOOLSDIR)
-pythonsh = '{0}bin/pythonsh'.format(MGLTOOLSDIR)
+pythonsh = "{0}bin/pythonsh".format(MGLTOOLSDIR)
 
 
 #COMMAND LINE PARSING#
 parser = argparse.ArgumentParser(prog='Vina_merge', usage='%(prog)s [options]')
 
 Input = parser.add_argument_group('Input')
-Input.add_argument('-w', '--worDir', action='store', help='working directory (default: vina output directory)', metavar='')
+Input.add_argument('-w', '--worDir', action='store', help='working directory (default: current working directory)', metavar='')
 Input.add_argument('-r', '--rigid', action='store', help='rigid PDBQT', required=True, metavar='')
 Input.add_argument('-p', '--poses', action='store', help='vina output PDBQT', required=True, metavar='')
 Input.add_argument('-n', '--number',  action='store', help='rank of the docking pose to merge (default: 1)', metavar='')
 
 Output = parser.add_argument_group('Output')
-Output.add_argument('-o', '--outDir', action='store', help='output directory (default: vina output directory)', metavar='')
+Output.add_argument('-o', '--outDir', action='store', help='output directory (default: current working directory)', metavar='')
 
 args = parser.parse_args()
 
 #REQUIRED VALUES#
 rigid=args.rigid
 poses=args.poses
-nm_poses=os.path.basename(poses)
+nm_poses=path.splitext(path.basename(poses))[0]
 
 #DEFAULT VALUES#
 if args.worDir is None:
-    worDir=os.path.dirname(args.poses)+"/"
+    worDir=os.getcwd()
 else:
     worDir=args.worDir
 
@@ -53,14 +54,13 @@ else:
     number=args.number
 
 if args.outDir is None:
-    outDir=worDir
+    outDir=os.getcwd()
 else:
     outDir=args.outDir
 
 #SOME FUNCTIONS#
 def pdbqt2pdb(pdbqt):
     subprocess.run([pythonsh, utilities24+"pdbqt_to_pdb.py", "-f {0}".format(pdbqt), "-o {0}".format(pdbqt)[:-2]])
-
 
 #########################################
 ###(1)EXTRACT THE MODEL YOU WANT MERGE###
@@ -71,7 +71,7 @@ with open (poses, "r") as fi:
             model_n = l.split()[0] + l.split()[1]
             j=i #remember starting position
             #extract pose n
-            with open ("{0}{1}_{2}.pdbqt".format(worDir,nm_poses,model_n), "w") as model:
+            with open (path.join(worDir, ("{0}_{1}.pdbqt".format(nm_poses,model_n))), "w") as model:
                 for k in range(j, len(lines)):
                     if lines[k].startswith("ENDMDL") == False:
                         model.write(lines[k])
@@ -114,9 +114,10 @@ with open (rigidpdb, "r") as fi:
                 if linef[17:26] == liner[17:26]:
                     merge.write(linef+"\n") #all lines of the flex residues matched
                     reslist.remove(liner[17:26]) #remove the flex from the list
-        if liner.startswith("TER"):
-            merge.write(lig.getvalue()) #merge the ligand at the end
-
+        #if liner.startswith("TER"):
+        #    continue
+    merge.write(lig.getvalue()) #merge the ligand at the end
+    merge.write("END")
 ###(6)RENUMBERING###
 atnum=1
 remerge=StringIO()
@@ -136,6 +137,6 @@ for line in (merge.getvalue()).split("\n"):
         remerge.write(line+"\n")
 
 ###(7)WRITE MERGED FILE###
-with open ("{0}{1}_merge.pdb".format(outDir,nm_poses), "w") as fo:
+with open (path.join(outDir, ("{0}_merge.pdbqt".format(nm_poses))), "w") as fo:
     cont = remerge.getvalue()
     fo.write(cont)
